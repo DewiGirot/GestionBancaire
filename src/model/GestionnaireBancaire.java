f0dc11048877c68202e0d41c3e19d9ca926a6557
Déwi <dewi.girot@gmail.com>
2023-03-31 15:44:39 +0200

    Chargertaux done, chargerTransaction on going

--- src/model/GestionnaireBancaire.java
+++ src/model/GestionnaireBancaire.java
@@ -5,8 +5,15 @@
 package model;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -26,6 +33,7 @@ public class GestionnaireBancaire {
 		this.solde = solde;
 		
 		this.chargerTaux();
+		this.chargerTransactions();
 	}
 
 
@@ -41,6 +49,44 @@ public class GestionnaireBancaire {
 	        System.err.format("Erreur de lecture de fichier: %s%n", e);
 	    } catch (IllegalArgumentException e) {
 	        System.err.format("Erreur dans le fichier taux.txt: %s%n", e);
+	    }
+	}
+	
+	public void chargerTransactions() {
+	    Path filePath = Paths.get("../GestionBancaire/src/data", "SaveList.bin");
+	    try (FileInputStream fis = new FileInputStream(filePath.toFile());
+	        ObjectInputStream ois = new ObjectInputStream(fis)) {
+	    	List<Transaction> loadedList = (LinkedList<Transaction>) ois.readObject();
+	    	listTransaction.clear();
+	        if(loadedList != null && !loadedList.isEmpty()) {
+	            listTransaction.addAll(loadedList);
+	        }
+	    } catch (IOException | ClassNotFoundException e) {
+	        System.err.println("Erreur lors du chargement des transactions : " + e.getMessage());
+	    }
+	}
+	
+//	public void sauvegarderTransactions() {
+//		try {
+//	        FileOutputStream fos = new FileOutputStream("../GestionBancaire/src/data/SaveList.bin");
+//	        ObjectOutputStream oos = new ObjectOutputStream(fos);
+//	        List<Transaction> saveList = new LinkedList<>(listTransaction);
+//	        oos.writeObject(saveList);
+//	        oos.close();
+//	        fos.close();
+//	    } catch (Exception e) {
+//	        System.err.println("Erreur lors de la sauvegarde des transactions : " + e.getMessage());
+//	    }
+//	}
+	
+	public void sauvegarderTransactions() {
+	    Path filePath = Paths.get("../GestionBancaire/src/data", "SaveList.bin");
+	    List<Transaction> saveList = new LinkedList<>(listTransaction);
+	    try (FileOutputStream fos = new FileOutputStream(filePath.toFile());
+	        ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+	        oos.writeObject(saveList);
+	    } catch (IOException e) {
+	        System.err.println("Erreur lors de la sauvegarde des transactions : " + e.getMessage());
 	    }
 	}
 
