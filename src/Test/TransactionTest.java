c801ab437ef078024e5e2e4da20b8cb11c022d3b
Déwi <dewi.girot@gmail.com>
2023-03-31 14:24:39 +0200

    Test creation invalid montant

--- src/Test/TransactionTest.java
+++ src/Test/TransactionTest.java
@@ -1,16 +1,21 @@
 package Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
+import org.junit.jupiter.api.Test;
 import model.Transaction;
 
 class TransactionTest {
 
 	@Test
-	void testCreationTransaction() {
-		String type = "c";
-		Transaction t = new Transaction("Transaction 1", type, 150.0);
+	public void testCreationTransaction() {
+		Transaction t = new Transaction("Transaction 1", "c", 150.0);
 	}
+	
+	@Test
+    public void testInvalidMontant() {
+		Assertions.assertThrows(IllegalArgumentException.class,
+            () -> new Transaction("frais", "débit", 0.0));
+    }
 
 }
